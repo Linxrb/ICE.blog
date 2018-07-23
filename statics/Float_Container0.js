43f56c7c3fad747183246ab3cf50fe414b019931
ice <13266665446@wo.cn>
2018-07-23 16:48:18 +0800

    update

--- statics/Float_Container0.js
+++ statics/Float_Container0.js
@@ -11,8 +11,8 @@ for (var v = 0; v < Agents.length; v++) {
 //图片的过程中，横坐标的轨迹是以一点为中心的正弦曲线
 //利用了setTimeout函数完成了动画的功能
 //pc端雪花个数
-var no = [0,1,2,3,4,5];
-if (flag){no = [0,1,2,3,4];}    //设备端
+var no = 6;
+if (flag){no = 5;}    //设备端
  
 //声明变量，xp表示横坐标，yp表示纵坐标>
 var dx, xp, yp;
@@ -32,8 +32,7 @@ var am = new Array();
 var stx = new Array();
 var sty = new Array();
 var snowFlakes = new Array();
-
-for (var i in no) {  
+for (i = 0; i < no; ++ i) {  
   //图片
   var imagesSrc=['0.gif','1.gif','2.gif','3.gif'];//img数据
   var t=Math.random()*12%3; //返回一个0-3的数(随机数)
@@ -46,13 +45,12 @@ for (var i in no) {
   am[i] = Math.random()*5;         //第i个图片的左右摆动的幅度
   stx[i] = 0.02 + Math.random()/10; //第i个图片x方向的步长
   sty[i] = 0.7 + Math.random();     //第i个图片y方向的步长
-  if(flag){sty[i] = 7 + Math.random()}
   //生成一个容纳雪花图片的div，并设置其绝对坐标
   var snowFlakeDiv = document.createElement('div');
   snowFlakeDiv.setAttribute('id', 'dot'+ i);
   snowFlakeDiv.style.position = 'fixed';
-  snowFlakeDiv.style.top = yp[i] + 'px';
-  snowFlakeDiv.style.left = xp[i] +'px';
+  snowFlakeDiv.style.top = 15+ 'px';
+  snowFlakeDiv.style.left = 15 +'px';
   //生成一个雪花图片对象，设置宽高，并加入div
   var snowFlakeImg = document.createElement('img');
   snowFlakeImg.setAttribute('src', snowsrc);
@@ -65,24 +63,32 @@ for (var i in no) {
 }
 function snow() {  
 
-  for (var i in no) {  
+  for (i = 0; i < no; ++ i) {  
     //第i个图片的纵坐标加上步长
     yp[i] = yp[i] + sty[i];
-    //dx变量加上一个步长
-    dx[i] = dx[i] + stx[i];
+    //如果新坐标超过了屏幕下沿，重置该图片的信息，包括横坐标、纵坐标以及x方向的步长和y方向的步长
+    if (yp[i] > clientHeight) {
+      //重新赋值图片的横坐标
+      xp[i] = Math.random()*(clientWidth-am[i]-30);
+      //重新赋值图片的纵坐标
+      yp[i] = 0;
+    }
+    clientWidth =  document.body.clientWidth;
+    clientHeight = document.body.clientHeight;
+    if (xp[i] > clientWidth) {
+      //重新赋值图片的横坐标
+      xp[i] = Math.random()*(clientWidth-am[i]-30);
+      //重新赋值图片的纵坐标
+      yp[i] = 0;
+    }
+    dx[i] = dx[i] + stx[i];//dx变量加上一个步长
     //直接操作数组中对应的雪花div
     var snowFlakeDiv = snowFlakes[i];
     //更新图片的纵坐标
     snowFlakeDiv.style.top = yp[i] +'px';
     //更新图片的横坐标
     snowFlakeDiv.style.left = xp[i] + am[i] * Math.sin(dx[i]) + 'px';
-    if(parseInt(snowFlakeDiv.style.top) > clientHeight){
-        if(no.length==0){return;}
-        delete no[i]
-        document.body.removeChild(snowFlakeDiv)
-        }
   }
-  
   //设定动画刷新的时间周期
   setTimeout("snow()", 100);
 }
